425ac67518f93f06489cbc62f9e9cb9a38917b5a
Jesus Humberto Morales <humberto.morales.14@hotmail.com>
2025-04-19 16:33:24 -0700

    🚧 Se agrego que se puedan agregar tareas, se configuro el api para que mande la peticion al agregar

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
-export const getAllTasks = () => {
-  return axios.get('http://127.0.0.1:8000/tasks/api/v1/tasks/')
-}
+// Se puede crear una instancia de axios para evitar repetir la url base
+const taksApi = axios.create({
+  baseURL: 'http://127.0.0.1:8000/tasks/api/v1/tasks/',
+});
+
+export const getAllTasks = () => taksApi.get('/');
+
+export const createTask = (task) => taksApi.post('/', task);
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,51 @@
+import { useForm } from 'react-hook-form';
+import { createTask } from '../api/tasks.api';
+import { useNavigate } from 'react-router-dom';
+
 export function TaskFormPage() {
+  // register: Permite registrar que inputs y sus validaciones
+  // handleSubmit: Permite manejar el submit del formulario
+  // formState: Permite manejar el estado del formulario
+  // errors: Permite manejar los errores del formulario
+  // useForm: Permite manejar el formulario
+  // Algunas bibliotecas adicionales seria yup o zod para manejar la validación de los formularios
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  // Para navegar entre las rutas del dom ya generadas
+  const navigate = useNavigate();
+
+  // Permite manejar los datos al hacer submit del formulario
+  // data: Contiene los datos del formulario
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await createTask(data);
+      navigate('/tasks');
+    } catch (error) {
+      console.error('Error creating task:', error);
+    }
+  });
+
   return (
-    <div>Task form page</div>
-  )
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="title"
+          {...register('title', { required: true })}
+        />
+        {errors.title && <span>Title is required</span>}
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register('description', { required: true })}
+        ></textarea>
+        {errors.description && <span>Description is required</span>}
+        <button type="submit">Create Task</button>
+      </form>
+    </div>
+  );
 }
